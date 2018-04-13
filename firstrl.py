6c41d3d4793c7ee7db6e383efdee5a12de754a6a
Theodor Tonum <theodor@tonum.no>
2018-04-13 22:18:52 +0200

    Add map with unwalkable tiles

--- firstrl.py
+++ firstrl.py
@@ -5,6 +5,24 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_dark_ground = libtcod.Color(50, 50, 150)
+
+
+class Tile:
+    # a tile of the map and its properties
+    def __init__(self, blocked, block_sight=None):
+        self.blocked = blocked
+
+        # by default, if a tile is blocked, it also blocks sight
+        if block_sight is None:
+            block_sight = blocked
+        self.block_sight = block_sight
+
+
 libtcod.console_set_custom_font(
     'arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT,
@@ -15,6 +33,19 @@ libtcod.sys_set_fps(LIMIT_FPS)
 player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
 npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
 objects = [npc, player]
+
+
+def make_map():
+    global map
+
+    # fill map with "unblocked" tiles
+    map = [[Tile(False)
+            for y in range(MAP_HEIGHT)]
+           for x in range(MAP_WIDTH)]
+    map[30][22].blocked = True
+    map[30][22].block_sight = True
+    map[50][22].blocked = True
+    map[50][22].block_sight = True
 
 
 def handle_keys():
@@ -29,23 +60,37 @@ def handle_keys():
 
     # movement keys
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        player.move(0, -1)
+        player.move(0, -1, map)
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        player.move(0, 1)
+        player.move(0, 1, map)
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        player.move(-1, 0)
+        player.move(-1, 0, map)
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        player.move(1, 0)
+        player.move(1, 0, map)
 
 
-while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(con, libtcod.white)
+def render_all():
     for object in objects:
         object.draw(con)
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            wall = map[x][y].block_sight
+            if wall:
+                libtcod.console_set_char_background(
+                    con, x, y, color_dark_wall, libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(
+                    con, x, y, color_dark_ground, libtcod.BKGND_SET)
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+
+make_map()
+while not libtcod.console_is_window_closed():
+    libtcod.console_set_default_foreground(con, libtcod.white)
+    render_all()
     libtcod.console_flush()
 
     # handle keys and exit game if needed
