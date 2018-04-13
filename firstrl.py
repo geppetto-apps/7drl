800dd15e0552d2eaee444ada6c7e7dd95622f4f0
Theodor Tonum <theodor@tonum.no>
2018-04-13 22:05:03 +0200

    Render in sub-console

--- firstrl.py
+++ firstrl.py
@@ -6,6 +6,7 @@ LIMIT_FPS = 20
 
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 libtcod.sys_set_fps(LIMIT_FPS)
 
 playerx = SCREEN_WIDTH/2
@@ -35,12 +36,13 @@ def handle_keys():
         playerx += 1
 
 while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(0, libtcod.white)
-    libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
+    libtcod.console_set_default_foreground(con, libtcod.white)
+    libtcod.console_put_char(con, playerx, playery, '@', libtcod.BKGND_NONE)
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
     libtcod.console_flush()
 
     #handle keys and exit game if needed
-    libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
+    libtcod.console_put_char(con, playerx, playery, ' ', libtcod.BKGND_NONE)
     exit = handle_keys()
     if exit:
         break
