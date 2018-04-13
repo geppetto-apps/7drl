75ab667fd237313004a359bcd0968009fca6a467
Theodor Tonum <theodor@tonum.no>
2018-04-13 22:18:21 +0200

    :lipstick::dancer:

--- object.py
+++ object.py
@@ -1,24 +1,27 @@
 import libtcodpy as libtcod
 
+
 class Object:
-    #this is a generic object: the player, a monster, an item, the stairs...
-    #it's always represented by a character on screen.
+    # this is a generic object: the player, a monster, an item, the stairs...
+    # it's always represented by a character on screen.
     def __init__(self, x, y, char, color):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
 
-    def move(self, dx, dy):
-        #move by the given amount
-        self.x += dx
-        self.y += dy
+    def move(self, dx, dy, map):
+        if not map[self.x + dx][self.y + dy].blocked:
+            # move by the given amount
+            self.x += dx
+            self.y += dy
 
     def draw(self, con):
-        #set the color and then draw the character that represents this object at its position
+        # set the color and then draw the character that represents this object at its position
         libtcod.console_set_default_foreground(con, self.color)
-        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+        libtcod.console_put_char(
+            con, self.x, self.y, self.char, libtcod.BKGND_NONE)
 
     def clear(self, con):
-        #erase the character that represents this object
+        # erase the character that represents this object
         libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
