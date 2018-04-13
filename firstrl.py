1c75bc26eda0869f506265728e541c29f9748d12
Theodor Tonum <theodor@tonum.no>
2018-04-13 22:42:31 +0200

    Move dungeon generator code to Map

--- firstrl.py
+++ firstrl.py
@@ -1,6 +1,5 @@
 import libtcodpy as libtcod
 from object import Object
-from rect import Rect
 from map import Map
 
 SCREEN_WIDTH = 80
@@ -22,75 +21,15 @@ player = Object(25, 23, '@', libtcod.white)
 npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
 objects = [npc, player]
 
-ROOM_MAX_SIZE = 10
-ROOM_MIN_SIZE = 6
-MAX_ROOMS = 30
 
 def make_map():
     global map
-    rooms = []
-    num_rooms = 0
+
     map = Map(MAP_WIDTH, MAP_HEIGHT)
-
-    for r in range(MAX_ROOMS):
-        #random width and height
-        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-        #random position without going out of the boundaries of the map
-        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
-        y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
-
-        #"Rect" class makes rectangles easier to work with
-        new_room = Rect(x, y, w, h)
-
-        #run through the other rooms and see if they intersect with this one
-        failed = False
-        for other_room in rooms:
-            if new_room.intersect(other_room):
-                failed = True
-                break
-
-        if not failed:
-            #this means there are no intersections, so this room is valid
-
-            #"paint" it to the map's tiles
-            map.create_room(new_room)
-
-            #center coordinates of new room, will be useful later
-            (new_x, new_y) = new_room.center()
-
-            if num_rooms == 0:
-                #this is the first room, where the player starts at
-                player.x = new_x
-                player.y = new_y
-            else:
-                #all rooms after the first:
-                #connect it to the previous room with a tunnel
-
-                #center coordinates of previous room
-                (prev_x, prev_y) = rooms[num_rooms-1].center()
-
-                #draw a coin (random number that is either 0 or 1)
-                if libtcod.random_get_int(0, 0, 1) == 1:
-                    #first move horizontally, then vertically
-                    map.create_h_tunnel(prev_x, new_x, prev_y)
-                    map.create_v_tunnel(prev_y, new_y, new_x)
-                else:
-                    #first move vertically, then horizontally
-                    map.create_v_tunnel(prev_y, new_y, prev_x)
-                    map.create_h_tunnel(prev_x, new_x, new_y)
-
-            #finally, append the new room to the list
-            rooms.append(new_room)
-            num_rooms += 1
-
-    # # fill map with "unblocked" tiles
-    # map = Map(MAP_WIDTH, MAP_HEIGHT)
-    # room1 = Rect(20, 15, 10, 15)
-    # room2 = Rect(50, 15, 10, 15)
-    # map.create_room(room1)
-    # map.create_room(room2)
-    # map.create_h_tunnel(25, 55, 23)
+    map.generate()
+    (x, y) = map.rooms[0].center()
+    player.x = x
+    player.y = y
 
 
 def handle_keys():
--- map.py
+++ map.py
@@ -1,8 +1,12 @@
 import libtcodpy as libtcod
+from rect import Rect
 
 color_dark_wall = libtcod.Color(0, 0, 100)
 color_dark_ground = libtcod.Color(50, 50, 150)
 
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
 
 class Tile:
     # a tile of the map and its properties
@@ -19,6 +23,8 @@ class Map:
     def __init__(self, w, h):
         self.w = w
         self.h = h
+        self.rooms = []
+        self.num_rooms = 0
         self.tiles = [[Tile(True)
                        for y in range(self.h)]
                       for x in range(self.w)]
@@ -40,6 +46,55 @@ class Map:
         for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
+
+    def generate(self):
+        for r in range(MAX_ROOMS):
+            #random width and height
+            w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+            h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+            #random position without going out of the boundaries of the map
+            x = libtcod.random_get_int(0, 0, self.w - w - 1)
+            y = libtcod.random_get_int(0, 0, self.h - h - 1)
+
+            #"Rect" class makes rectangles easier to work with
+            new_room = Rect(x, y, w, h)
+
+            #run through the other rooms and see if they intersect with this one
+            failed = False
+            for other_room in self.rooms:
+                if new_room.intersect(other_room):
+                    failed = True
+                    break
+
+            if not failed:
+                #this means there are no intersections, so this room is valid
+
+                #"paint" it to the map's tiles
+                self.create_room(new_room)
+
+                #center coordinates of new room, will be useful later
+                (new_x, new_y) = new_room.center()
+
+                if self.num_rooms != 0:
+                    #all rooms after the first:
+                    #connect it to the previous room with a tunnel
+
+                    #center coordinates of previous room
+                    (prev_x, prev_y) = self.rooms[self.num_rooms-1].center()
+
+                    #draw a coin (random number that is either 0 or 1)
+                    if libtcod.random_get_int(0, 0, 1) == 1:
+                        #first move horizontally, then vertically
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+                    else:
+                        #first move vertically, then horizontally
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+
+                #finally, append the new room to the list
+                self.rooms.append(new_room)
+                self.num_rooms += 1
 
     def tile_at(self, x, y):
         return self.tiles[x][y]
