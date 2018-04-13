632dce62fc41968281b4e10456d44b12792c2bfb
Theodor Tonum <theodor@tonum.no>
2018-04-13 22:10:10 +0200

    Add NPC with object abstraction

--- firstrl.py
+++ firstrl.py
@@ -1,4 +1,5 @@
 import libtcodpy as libtcod
+from object import Object
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
@@ -9,11 +10,12 @@ libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial'
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 libtcod.sys_set_fps(LIMIT_FPS)
 
-playerx = SCREEN_WIDTH/2
-playery = SCREEN_HEIGHT/2
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+objects = [npc, player]
 
 def handle_keys():
-    global playerx, playery
+    global player
     key = libtcod.console_wait_for_keypress(True)
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         #Alt+Enter: toggle fullscreen
@@ -24,25 +26,27 @@ def handle_keys():
 
     #movement keys
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        playery -= 1
+        player.move(0, -1)
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        playery += 1
+        player.move(0, 1)
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        playerx -= 1
+        player.move(-1, 0)
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        playerx += 1
+        player.move(1, 0)
 
 while not libtcod.console_is_window_closed():
     libtcod.console_set_default_foreground(con, libtcod.white)
-    libtcod.console_put_char(con, playerx, playery, '@', libtcod.BKGND_NONE)
+    for object in objects:
+        object.draw(con)
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
     libtcod.console_flush()
 
     #handle keys and exit game if needed
-    libtcod.console_put_char(con, playerx, playery, ' ', libtcod.BKGND_NONE)
+    for object in objects:
+        object.clear(con)
     exit = handle_keys()
     if exit:
         break
