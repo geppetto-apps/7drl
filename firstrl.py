ec95932f601dd09d76e51c01f4fb143c2d4196c9
Theodor Tonum <theodor@tonum.no>
2018-04-14 10:50:27 +0200

    Implement game state

--- firstrl.py
+++ firstrl.py
@@ -9,6 +9,8 @@ LIMIT_FPS = 20
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
 
+game_state = 'playing'
+player_action = None
 
 libtcod.console_set_custom_font(
     'arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -44,24 +46,28 @@ def handle_keys():
         libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
     elif key.vk == libtcod.KEY_ESCAPE:
-        return True  # exit game
+        return 'exit'  # exit game
 
-    # movement keys
-    if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        player.move(0, -1, map)
-        map.fov_recompute(player)
+    if game_state == 'playing':
+        # movement keys
+        if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+            player.move(0, -1, map)
+            map.fov_recompute(player)
 
-    elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        player.move(0, 1, map)
-        map.fov_recompute(player)
+        elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+            player.move(0, 1, map)
+            map.fov_recompute(player)
 
-    elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        player.move(-1, 0, map)
-        map.fov_recompute(player)
+        elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+            player.move(-1, 0, map)
+            map.fov_recompute(player)
 
-    elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        player.move(1, 0, map)
-        map.fov_recompute(player)
+        elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+            player.move(1, 0, map)
+            map.fov_recompute(player)
+
+        else:
+            return 'didnt-take-turn'
 
 
 def render_all():
@@ -80,6 +86,6 @@ while not libtcod.console_is_window_closed():
     # handle keys and exit game if needed
     for object in objects:
         object.clear(con)
-    exit = handle_keys()
-    if exit:
+    player_action = handle_keys()
+    if player_action == 'exit':
         break
