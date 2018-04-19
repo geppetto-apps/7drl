9812a2aa5e43b5aaf52b812394f425059d911de6
Theodor Tonum <theodor@tonum.no>
2018-04-19 20:03:54 +0200

    Implement A* pathfinding

--- components.py
+++ components.py
@@ -108,7 +108,7 @@ class BasicMonster:
 
             # move towards player if far away
             if monster.distance_to(player) >= 2:
-                monster.move_towards(player.x, player.y, map)
+                monster.move_astar(player, map)
 
             # close enough, attack! (if the player is still alive.)
             elif player.fighter.hp > 0:
